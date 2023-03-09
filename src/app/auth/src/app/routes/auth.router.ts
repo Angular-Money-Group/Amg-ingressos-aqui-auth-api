9f9560d5bd68a63470b8810a10ae82da885097ac
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2023-03-09 10:19:09 -0300

    Fix get logout

--- src/app/auth/src/app/routes/auth.router.ts
+++ src/app/auth/src/app/routes/auth.router.ts
@@ -5,11 +5,11 @@ import AuthController from "../controller/auth.controller";
 const authRouter = Router();
 
 authRouter.get("/getUser", AuthController.getUser);
+authRouter.get("/logout", AuthController.logout);
 authRouter.post("/login", AuthController.login);
 authRouter.post("/registerCustomer", AuthController.registerCustomer);
 authRouter.post("/registerProducer", AuthController.registerProducer);
 authRouter.post("/refreshToken", AuthController.refreshToken);
-authRouter.post("/logout", AuthController.logout);
 
 
 export default authRouter;
--- src/app/auth/src/app/utils/autenticateToken.utils.ts
+++ src/app/auth/src/app/utils/autenticateToken.utils.ts
@@ -1,20 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(" ")[1];
-    if (!token) {
-      return res.sendStatus(401);
-    }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (err: any, payload: any) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      next();
-    });
-  };
-
   export const generateTokens = (payload: any) => {
     const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET!, {
       expiresIn: "15m",
@@ -25,4 +11,4 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     return { accessToken, refreshToken };
   };
 
-export default { authenticateToken, generateTokens };
+export default { generateTokens };
