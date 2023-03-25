67311c1fcfcadb952ed8934e42beee977c38f35e
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2023-03-25 13:05:12 -0300

    Fix ADD convert Tickets area

--- src/app/auth/src/app/models/days.models.ts
+++ src/app/auth/src/app/models/days.models.ts
@@ -1,14 +1,14 @@
 import mongoose, { Document } from "mongoose";
 
 const daysModel = new mongoose.Schema({
-    start: { type: Date, required: true },
-    end: { type: Date, required: true },
+    start: { type: Date },
+    end: { type: Date },
 });
 
 export default mongoose.model<DaysType>("Days", daysModel);
 
 export interface DaysType extends Document{
-    start: string;
-    end: string;
+    start: Date;
+    end: Date;
   }
   
